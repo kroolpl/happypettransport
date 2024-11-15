5075eff410c40ce107c04645096c578384d21fc0
qrol <ggglondyn@gmail.com>
2024-11-16 00:12:06 +0100

    no perform

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
 
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    compress({
-      CSS: true,
-      HTML: true,
-      Image: false,
-      JavaScript: true,
-      SVG: true,
-      Logger: 1
-    })
-  ],
+  integrations: [tailwind()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "pl"],
